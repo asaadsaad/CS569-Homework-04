873e162571b09490871eed68c84d8aee9701c261
Tariku Gelan <tgelan@mum.edu>
2020-05-24 12:01:48 -0500

    final assig-5

--- assignment5/src/app/app.component.ts
+++ assignment5/src/app/app.component.ts
@@ -5,31 +5,63 @@ import { Component } from '@angular/core';
   template: `<h2> Rock Paper Scissors </h2>
   <button (click) = "rockClickHandler($event)"> Rock</button>
   <button (click) = "paperClickHandler($event)">Paper</button>
-  <button (click) = "scissorClickHandler($event)"> Sciccor</button>
-  <h3> Score: Win  = {{win}}  /Lose {{lose}} </h3>
+  <button (click) = "scissorClickHandler($event)"> Scissor</button>
+  <h3> Score: Win  = {{game.win}}  / Lose {{game.lose}} </h3>
   `,
 })
 export class AppComponent {
-  win: number = 0
-  lose: number = 0
-  computer: string = "rock"
+  game = { win: 0, lose: 0, computer: "" }
+  score: number = 0
+  theComputer = () => {
+    const randomNum = Math.floor(Math.random() * 3)
+    const options: string[] = ['Rock', 'Paper', 'Scissor'];
+    return this.game.computer = options[randomNum]
+  }
+
   rockClickHandler = (e) => {
-    if (this.computer === "scissor") {
-      this.win++
+    const compPick = this.theComputer()
+    const playerPick = e.target.innerHTML
+    if (playerPick == compPick) {
+       alert("its a draw!")
+     
+    }
+    else if (playerPick !== compPick && compPick == "Scissor") {
+      this.game.win++
     }
     else {
-      this.win--
+      this.game.lose++
     }
   }
+
   paperClickHandler = (e) => {
-    if (this.computer === "scissor") {
-      this.win--
+    const compPick = this.theComputer()
+    const playerPick = e.target.innerHTML
+    if (playerPick == compPick) {
+      alert("its a draw!")
+    }
+    else if (playerPick !== compPick && compPick == "Scissor") {
+      this.game.lose++
     }
     else {
-      this.win++
+      this.game.win++
     }
   }
+
   scissorClickHandler = (e) => {
-    console.log('scissor')
+    const compPick = this.theComputer()
+    const playerPick = e.target.innerHTML
+    if (playerPick == compPick) {
+      alert("its a draw!")
+      console.log("draw")
+    }
+    else if (playerPick !== compPick && compPick == "Paper") {
+      this.game.win++
+    }
+    else {
+      this.game.lose++
+    }
   }
+
+
+  ngOnChanges(score) { }
 }
